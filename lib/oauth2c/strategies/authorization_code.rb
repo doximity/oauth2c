52ddb218ea7a0209cadaf9718dc486cffc04e83d
Rodrigo Kochenburger <divoxx@gmail.com>
2017-03-01 11:46:13 -0800

    Add license headers

--- lib/oauth2c/strategies/authorization_code.rb
+++ lib/oauth2c/strategies/authorization_code.rb
@@ -1,29 +1,40 @@
-module OAuth2
-  module Client
-    module Strategies
-      module AuthorizationCode
-        class AuthzHandler < OAuth2c::AuthzHandler
-          def response_type
-            "code"
-          end
+# Copyright 2017 Doximity, Inc. <support@doximity.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+module OAuth2c
+  module Strategies
+    module AuthorizationCode
+      class AuthzHandler < OAuth2c::AuthzHandler
+        def response_type
+          "code"
+        end
+      end
+
+      class TokenHandler < OAuth2c::TokenHandler
+        def self.from_authz_callback_params(params)
+          new(params["code"])
         end
 
-        class TokenHandler < OAuth2c::TokenHandler
-          def self.from_authz_callback_params(params)
-            new(params["code"])
-          end
+        def initialize(code)
+          @code = code
+        end
 
-          def initialize(code)
-            @code = code
-          end
+        def grant_type
+          "authorization_code"
+        end
 
-          def grant_type
-            "authorization_code"
-          end
-
-          def extra_params
-            { code: @code }
-          end
+        def extra_params
+          { code: @code }
         end
       end
     end
--- lib/oauth2c/strategies/resource_owner_credentials.rb
+++ lib/oauth2c/strategies/resource_owner_credentials.rb
@@ -1,20 +1,31 @@
-module OAuth2
-  module Client
-    module Strategies
-      module ResourceOwnerCredentials
-        class TokenHandler < OAuth2c::TokenHandler
-          def initialize(username, password)
-            @username = username
-            @password = password
-          end
+# Copyright 2017 Doximity, Inc. <support@doximity.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+module OAuth2c
+  module Strategies
+    module ResourceOwnerCredentials
+      class TokenHandler < OAuth2c::TokenHandler
+        def initialize(username, password)
+          @username = username
+          @password = password
+        end
 
-          def grant_type
-            "password"
-          end
+        def grant_type
+          "password"
+        end
 
-          def extra_params
-            { username: @username, password: @password }
-          end
+        def extra_params
+          { username: @username, password: @password }
         end
       end
     end
